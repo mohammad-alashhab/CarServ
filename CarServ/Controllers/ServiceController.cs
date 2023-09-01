827799a7aabfaeaafbf717f07c84b05f37eb136c
MANSOOR MOQBEL <mansoormoqbel124@outlook.com>
2023-09-01 20:47:11 +0300

    mansoor update controller

---
+++ CarServ/Controllers/ServiceController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarServ.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Service()
+        {
+            return View();
+        }
+        public IActionResult Booking()
+        {
+            return View();
+        }
+        public IActionResult Technicians()
+        {
+            return View();
+        }
+        public IActionResult Testimonial()
+        {
+            return View();
+        }
+    }
+}
