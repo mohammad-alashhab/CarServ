d92214577bf2372a787f970ebb612b26898c0f4d
MANSOOR MOQBEL <mansoormoqbel124@outlook.com>
2023-09-01 21:33:00 +0300

    mansoor create Read folder and service &view

---
+++ CarServ/Controllers/ReadMoreController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarServ.Controllers
+{
+    public class ReadMoreController : Controller
+    {
+        /*
+            /ReadMore/DiagnosticTest
+            /ReadMore/EngineServicing
+            /ReadMore/TiresReplacement
+            /ReadMore/OilChanging
+         
+         
+         
+         */
+        public IActionResult DiagnosticTest()
+        {
+            return View();
+        }
+        public IActionResult EngineServicing()
+        {
+            return View();
+        }
+        public IActionResult TiresReplacement()
+        {
+            return View();
+        }
+        public IActionResult OilChanging()
+        {
+            return View();
+        }
+    }
+}
