04ddd1cd60d8f47db4d2df6f2152d9372773fec1
mohmmadalashhab <85409727+mohmmadalashhab@users.noreply.github.com>
2023-09-02 21:06:22 +0300

    Mohammad add contact validation, model

--- CarServ/Models/Contact.cs
+++ CarServ/Models/Contact.cs
@@ -1,26 +1,20 @@
-﻿//using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CarServ.Models
 {
-                 
-    public class Booking
+    public class Contact
     {
         [Required]
         [RegularExpression(@"^[A-Za-z]{1,50}\s[A-Za-z]{1,50}$", ErrorMessage = "Invalid name format. Please use the format: 'First Name Last Name'")]
-        public string YourName { get; set; }
+        public string FullName { get; set; }
         [Required]
         //[EmailAddress(ErrorMessage = "Invalid Email Format")]
         [RegularExpression(@"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$", ErrorMessage = "Please enter a valid email address.")]
         public string Email { get; set; }
         [Required]
-        [RegularExpression(@"[A-Za-z]{1,50}", ErrorMessage = "cannot be used Symbols")]
-        public string SpecialRequest { get; set; }
+        [RegularExpression(@"[A-Za-z]{1,50}", ErrorMessage = "cannot be used Symbols or Numbers")]
+        public string Subject { get; set; }
         [Required]
-        public string ServiceDate { get; set; }
-        [Required]
-        public Center Center { get; set; }
-        [Required]
-        public Services Service { get; set; }
+        public string Message { get; set; }
     }
 }
