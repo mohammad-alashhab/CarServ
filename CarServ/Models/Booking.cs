e986aa0dadc3befc3856658b01ccdd3657cdb663
mohmmadalashhab <85409727+mohmmadalashhab@users.noreply.github.com>
2023-09-02 21:32:10 +0300

    Mohammad edit model and validation

--- CarServ/Models/Booking.cs
+++ CarServ/Models/Booking.cs
@@ -7,14 +7,14 @@ namespace CarServ.Models
     public class Booking
     {
         [Required]
-        [RegularExpression(@"^[A-Za-z]{1,50}\s[A-Za-z]{1,50}$", ErrorMessage = "Invalid name format. Please use the format: 'First Name Last Name'")]
+        [RegularExpression(@"[A-Za-z\s]{1,100}", ErrorMessage = "Invalid name format. Please use the format: 'First Name Last Name'")]
         public string YourName { get; set; }
         [Required]
         //[EmailAddress(ErrorMessage = "Invalid Email Format")]
         [RegularExpression(@"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$", ErrorMessage = "Please enter a valid email address.")]
         public string Email { get; set; }
         [Required]
-        [RegularExpression(@"[A-Za-z]{1,50}", ErrorMessage = "cannot be used Symbols")]
+        [RegularExpression(@"[A-Za-z0-9\s]{1,50}", ErrorMessage = "can't be used Symbols or Numbers")]
         public string SpecialRequest { get; set; }
         [Required]
         public string ServiceDate { get; set; }
--- CarServ/Models/Contact.cs
+++ CarServ/Models/Contact.cs
@@ -5,16 +5,17 @@ namespace CarServ.Models
     public class Contact
     {
         [Required]
-        [RegularExpression(@"^[A-Za-z]{1,50}\s[A-Za-z]{1,50}$", ErrorMessage = "Invalid name format. Please use the format: 'First Name Last Name'")]
+        [RegularExpression(@"[A-Za-z\s]{1,100}", ErrorMessage = "Invalid name format. Please use the format: 'First Name Last Name'")]
         public string FullName { get; set; }
         [Required]
         //[EmailAddress(ErrorMessage = "Invalid Email Format")]
         [RegularExpression(@"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$", ErrorMessage = "Please enter a valid email address.")]
         public string Email { get; set; }
         [Required]
-        [RegularExpression(@"[A-Za-z]{1,50}", ErrorMessage = "cannot be used Symbols or Numbers")]
+        [RegularExpression(@"[A-Za-z]{1,50}", ErrorMessage = "can't be used Symbols or Numbers")]
         public string Subject { get; set; }
         [Required]
+        [RegularExpression(@"[A-Za-z0-9\s]{1,500}", ErrorMessage = "Message Should Be Under 500 Charaters and can't be use Symbols")]
         public string Message { get; set; }
     }
 }
