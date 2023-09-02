f66d24cacea8d8096b395de797b60ab254755eaf
MANSOOR MOQBEL <mansoormoqbel124@outlook.com>
2023-09-02 19:35:30 +0300

    mansoor working  validation form Booking

--- CarServ/Models/Booking.cs
+++ CarServ/Models/Booking.cs
@@ -6,15 +6,15 @@ namespace CarServ.Models
                  
     public class Booking
     {
-        [Required(ErrorMessage = "user name cannot be Empty ")]
-        [RegularExpression(@"[A-Za-z]{1,100}[ ]{1}[A-Za-z]{1,100}", ErrorMessage = "invild name should comply with Example mansoor moqbel")]
+        [Required]
+        [RegularExpression(@"[A-Za-z]{1,100}", ErrorMessage = "invild name should comply with Example mansoor moqbel")]
         public string YourName { get; set; }
         [Required]
         //[EmailAddress(ErrorMessage = "Invalid Email Format")]
-        //[RegularExpression(@"[A-Za-z0-9]{1,100}[@]{1}[a-z]{2,9}[.]{1}[a-z]{3,5}", ErrorMessage = "Email format should comply with Example@info.com")]
+        [RegularExpression(@"[A-Za-z0-9]{1,100}[@]{1}[a-z]{2,9}[.]{1}[a-z]{3,5}", ErrorMessage = "Email format should comply with Example@info.com")]
         public string Email { get; set; }
         [Required]
-        //[RegularExpression(@"[A-Za-z]{1,200}", ErrorMessage = "cannot be used Symbols")]
+        [RegularExpression(@"[A-Za-z]{1,50}", ErrorMessage = "cannot be used Symbols")]
         public string SpecialRequest { get; set; }
         [Required]
         public string ServiceDate { get; set; }
