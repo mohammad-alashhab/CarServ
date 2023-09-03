34feeb21473d7882b43b56f09794bfa1e32be9a3
MANSOOR MOQBEL <mansoormoqbel124@outlook.com>
2023-09-03 20:10:35 +0300

    mansoor add TagHelper in pages

--- CarServ/Models/Booking.cs
+++ CarServ/Models/Booking.cs
@@ -14,8 +14,16 @@ namespace CarServ.Models
         [RegularExpression(@"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$", ErrorMessage = "Please enter a valid email address.")]
         public string Email { get; set; }
         [Required]
+        [RegularExpression("[A-Za-z0-9]{3,5}", ErrorMessage = "CodeDiscount has to be AA123 format ")]
+        public string CodeDiscount { get; set; }
+        [Required]
         [RegularExpression(@"[A-Za-z0-9\s]{1,50}", ErrorMessage = "can't be used Symbols or Numbers")]
         public string SpecialRequest { get; set; }
+        [DataType(DataType.Currency)]
+        public string Payment { get; set; }
+        [DataType(DataType.Currency)]
+        public string Car { get; set; }
+
         [Required]
         public string ServiceDate { get; set; }
         [Required]
